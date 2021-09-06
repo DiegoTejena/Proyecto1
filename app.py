189a296bc92afec38cccb65b1667d465b103c346
DiegoTejena <90220451+DiegoTejena@users.noreply.github.com>
2021-09-06 17:23:49 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+import pandas as pd 
+import lasio 
+st.title("Aplicación para registro de pozos")
+st.sidebar.title("Menú")
+opciones_inicio= st.sidebar.radio("Seleccione una opción",["Inicio","Datos","Calculos"])
+#llamar un archivo
+archivo_las= lasio.read("LGAE-040.las")
+df=archivo_las.df()
+
+if opciones_inicio == "Inicio":
+	st.write("Ingreso al Inicio")
+	st.write(df)
+if opciones_inicio == "Datos":
+	st.write("Ingreso a Datos")
+	barra_deslizadora= st.slider("Seleccione un valor",1,100,30)
+	st.write("El valor seleccionado es",barra_deslizadora)
+	ingreso_numero= st.number_input("Ingrese un valor",min_value=1.00,max_value=1000.00,value=300.00)
+if opciones_inicio == "Calculos":
+	st.write("Ingreso a Calculos")
+	seleccion= st.selectbox("Seleccione una opción",[1,2,3,"A"])
+	check1= st.checkbox("Activar")
+	if check1==True:
+		st.write("Check activo")
+archivo_subido=st.sidebar.file_uploader("Cargar archivo excel",type=[".xls",".xlsx"])
+if archivo_subido is not None:
+	df2=pd.read_excel(archivo_subido)
+	st.write(df2)
